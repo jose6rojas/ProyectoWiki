d68ae4a5c1685c4e064389306ac3cd5887c90c9a
Oswal-Fuentes <oswal_f@unitec.edu>
2019-03-03 09:21:58 -0600

    Router basico

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,11 @@
+<script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script>
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBTJhHRW60MqYY-mVSqX_UwPxVraZ1iVtc",
+    authDomain: "react-firebase-authentic-c4784.firebaseapp.com",
+    databaseURL: "https://react-firebase-authentic-c4784.firebaseio.com",
+    projectId: "react-firebase-authentic-c4784",
+    storageBucket: "react-firebase-authentic-c4784.appspot.com",
+    messagingSenderId: "809203984683"
+};
+firebase.initializeApp(config);
---
+++ src/components/Firebase/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Fbase = () => (
+  <div>
+    <h1>Firebase</h1>
+  </div>
+);
+
+export default Fbase;
