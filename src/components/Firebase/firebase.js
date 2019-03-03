f2efcdf8e4407722cc9cf0d13160e36e815355ec
Oswal-Fuentes <oswal_f@unitec.edu>
2019-03-03 15:09:28 -0600

    Firebase's Authentication API

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 //Prod
 //<script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script>
@@ -33,7 +34,20 @@ const config =
 class Firebase {
     constructor() {
         app.initializeApp(config);
+        this.auth = app.auth();
     }
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
