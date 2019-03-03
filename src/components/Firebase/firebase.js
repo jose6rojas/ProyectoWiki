c8a1f5d8585433c1f8693689fecbb06596b5e05f
Oswal-Fuentes <oswal_f@unitec.edu>
2019-03-03 13:33:14 -0600

    Conexion React-Firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,23 @@
-<script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script>
+import app from 'firebase/app';
+
+//Prod
+//<script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script>
 // Initialize Firebase
-var config = {
+var prodConfig = {
+    apiKey: "AIzaSyA_UpAlU6OFP8C_14s7Oc1VwMdCDySG4O8",
+    authDomain: "wiki-prod.firebaseapp.com",
+    databaseURL: "https://wiki-prod.firebaseio.com",
+    projectId: "wiki-prod",
+    storageBucket: "wiki-prod.appspot.com",
+    messagingSenderId: "144106417596"
+};
+//firebase.initializeApp(prodConfig);
+
+
+//Dev
+//<script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script>
+// Initialize Firebase
+var devConfig = {
     apiKey: "AIzaSyBTJhHRW60MqYY-mVSqX_UwPxVraZ1iVtc",
     authDomain: "react-firebase-authentic-c4784.firebaseapp.com",
     databaseURL: "https://react-firebase-authentic-c4784.firebaseio.com",
@@ -8,4 +25,15 @@ var config = {
     storageBucket: "react-firebase-authentic-c4784.appspot.com",
     messagingSenderId: "809203984683"
 };
-firebase.initializeApp(config);
+//firebase.initializeApp(devConfig);
+
+const config =
+    process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+    }
+}
+
+export default Firebase;
--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
+import FirebaseContext from './context';
+import Firebase from './firebase';
 
-const Fbase = () => (
-  <div>
-    <h1>Firebase</h1>
-  </div>
-);
+export default Firebase;
 
-export default Fbase;
+export { FirebaseContext };
