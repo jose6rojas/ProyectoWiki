7ae83ddb7dd723d43ffe73cae424f041c60a3d1b
abialvarado14 <suyapa.caldex@hotmail.es>
2019-03-11 09:34:48 -0600

    Conexión con home page

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -19,7 +19,6 @@ import * as ROUTES from '../../constants/routes';
 const App = () => (
   <Router>
     <div>
-      <Navigation />
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-//import NavBar from '../Navigation/NavBar';
+import NavBar from '../Navigation/NavBar';
 import Inicio from './Inicio';
-import Home from '../InPage/HomeP.js';
 
 class Landing extends Component {
   render() {
     return (
       <div className="App">
-          {/*}<NavBar/>{*/}
+          <NavBar/>
           <Inicio/>
-         {/* <div className = "margen"> 
-            <Home/>
-    </div>*/}
       </div>
     );
   }
