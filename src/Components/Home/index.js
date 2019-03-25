7c982ca39c579d723b98b85ef89de449c2d4031e
abialvarado14 <suyapa.caldex@hotmail.es>
2019-03-24 19:11:18 -0600

    Página de Inicio terminada

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -5,6 +5,14 @@ import computer from '../Pics/computer.png';
 import build from '../Pics/build.png';
 import math from '../Pics/math.png';
 import book from '../Pics/books3.jpg';
+import java from '../Pics/java.jpg';
+import ux from '../Pics/UX.png';
+import c from '../Pics/c.jpg';
+import jav from '../Pics/jav.jpg';
+import so from '../Pics/so.jpg';
+import sql from '../Pics/sql.jpg';
+import nosql from '../Pics/nosql.jpg';
+import redes from '../Pics/redes.jpg';
 import { withAuthorization } from '../Session';
 
 class Home extends React.Component {
@@ -55,6 +63,72 @@ class Home extends React.Component {
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <h1>Clases de Desarrollo de Aplicaciones</h1>
+                
+                <div className="card-group">
+                    <div className="card border-primary text-white bg-dark mb-5">
+                         <img src={java} width="40" height="40" className="card-img-top centrado" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title centrado">Programación I</h5>
+                            <p className="card-text">Abi Alvarado </p>
+                         </div>
+                    </div>
+                    <div className="card border-primary text-white bg-dark mb-5">
+                         <img src={ux} width="40" height="40" className="card-img-top centrado" alt="..."/>
+                         <div className="card-body">
+                            <h5 className="card-title">Experiencia de Usuario</h5>
+                            <p className="card-text">José Rojas</p>
+                        </div>
+                    </div>
+                    <div className="card border-primary text-white bg-dark mb-5">
+                         <img src={c} width="40" height="40" className="card-img-top centrado" alt="..."/>
+                         <div className="card-body">
+                            <h5 className="card-title">Programación III</h5>
+                            <p className="card-text">Oswal Fuentes</p>
+                        </div>
+                     </div>
+                     <div className="card border-primary text-white bg-dark mb-5">
+                         <img src={jav} width="40" height="40" className="card-img-top centrado" alt="..."/>
+                         <div className="card-body">
+                            <h5 className="card-title">Lab. Programación II</h5>
+                            <p className="card-text">Oswal Fuentes</p>
+                        </div>
+                     </div>
+                </div>
+
+                <h1>Clases de Infraestructura</h1>
+                
+                <div className="card-group">
+                    <div className="card border-primary text-white bg-dark mb-5">
+                         <img src={so} width="40" height="40" className="card-img-top centrado" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title centrado">Sistemas Operativos I</h5>
+                            <p className="card-text">José Rojas</p>
+                         </div>
+                    </div>
+                    <div className="card border-primary text-white bg-dark mb-5">
+                         <img src={sql} width="40" height="40" className="card-img-top centrado" alt="..."/>
+                         <div className="card-body">
+                            <h5 className="card-title">TeorÍa de Base de Datos I</h5>
+                            <p className="card-text">Abi Alvarado</p>
+                        </div>
+                    </div>
+                    <div className="card border-primary text-white bg-dark mb-5">
+                         <img src={nosql} width="40" height="40" className="card-img-top centrado" alt="..."/>
+                         <div className="card-body">
+                            <h5 className="card-title">Teoría de Base de Datos II</h5>
+                            <p className="card-text">Oswal Fuentes</p>
+                        </div>
+                     </div>
+                     <div className="card border-primary text-white bg-dark mb-5">
+                         <img src={redes} width="40" height="40" className="card-img-top centrado" alt="..."/>
+                         <div className="card-body">
+                            <h5 className="card-title">Redes</h5>
+                            <p className="card-text">Oswal Fuentes</p>
+                        </div>
+                     </div>
                 </div>
 
             </div>
